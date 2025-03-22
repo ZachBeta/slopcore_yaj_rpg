ce2416b020dc3a0f24d852300e6318d07a48725f
Zach Morek <zmorek@gmail.com>
2025-03-22 11:16:31 -0400

    foo

--- checkpoint_commit.sh
+++ checkpoint_commit.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# default message
+message="checkpoint"
+
+# if an argument is provided, use it as the message
+if [ -n "$1" ]; then
+    message="$1"
+fi
+
 git add .
-git commit -m "checkpoint"
+git commit -m "$message"
 
